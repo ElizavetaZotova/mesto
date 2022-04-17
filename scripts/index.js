41969177294f57f2b39bb5e1bcd834205299937e
Elizaveta Zotova <e.zotova@dodopizza.com>
2022-04-17 15:15:45 +0500

    feat: реализовать логику добавления карточки через попап

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,15 @@ const formElement = document.querySelector('#popupForm');
 const nameInput = document.querySelector('#userNameInput');
 const jobInput = document.querySelector('#userAboutInput');
 
+const addElementForm = document.querySelector('#addNewPopupForm');
+const placeNameInput = document.querySelector('#placeNameInput');
+const placeImageLinkInput = document.querySelector('#placeImageLinkInput');
+
 const profileEditButton = document.querySelector('#profileEditButton');
+const profileAddButton = document.querySelector('.profile__add-button')
 const editProfilePopup = document.querySelector('#editProfilePopup');
-const editProfilePopupCloseButton = document.querySelector('#editProfilePopupCloseButton');
+const addNewElementPopup = document.querySelector('#addNewElementPopup');
+const popupsCloseButtons = document.querySelectorAll('.popup__close-button');
 
 const profileNameElement = document.querySelector('.profile__name');
 const profileDescriptionElement = document.querySelector('.profile__discription');
@@ -47,7 +53,7 @@ function renderElements() {
 
   removeAllChildNodes(elementsContainer);
 
-    for(const card of cardsList) {
+  for (const card of cardsList) {
     // клонируем содержимое тега template
     const elementItem = elementTemplate.querySelector('.elements__item').cloneNode(true);
 
@@ -76,8 +82,13 @@ function openEditProfilePopup() {
   jobInput.value = currentJobValue;
 }
 
-function closeEditProfilePopup() {
+function closePopups() {
   editProfilePopup.classList.remove('popup_opened');
+  addNewElementPopup.classList.remove('popup_opened');
+}
+
+function openAddNewElementPopup() {
+  addNewElementPopup.classList.add('popup_opened');
 }
 
 function formSubmitHandler(evt) {
@@ -86,11 +97,26 @@ function formSubmitHandler(evt) {
   profileNameElement.textContent = nameInput.value;
   profileDescriptionElement.textContent = jobInput.value;
 
-  closeEditProfilePopup();
+  closePopups();
+}
+
+function addElementFormSubmitHandler(evt) {
+  evt.preventDefault();
+
+  cardsList.unshift({
+    name: placeNameInput.value,
+    link: placeImageLinkInput.value,
+  });
+
+  closePopups();
+  renderElements();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+addElementForm.addEventListener('submit', addElementFormSubmitHandler);
 profileEditButton.addEventListener('click', openEditProfilePopup);
-editProfilePopupCloseButton.addEventListener('click', closeEditProfilePopup);
+profileAddButton.addEventListener('click', openAddNewElementPopup);
+
+popupsCloseButtons.forEach(button => button.addEventListener('click', closePopups));
 
 renderElements();
