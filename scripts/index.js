8c01ac3a27bac2b9a5c29a94e387f79cbac1a55c
Elizaveta Zotova <e.zotova@dodopizza.com>
2022-04-17 14:52:12 +0500

    feat: добавить рендер элементов карточек JS

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,63 @@ const editProfilePopupCloseButton = document.querySelector('#editProfilePopupClo
 const profileNameElement = document.querySelector('.profile__name');
 const profileDescriptionElement = document.querySelector('.profile__discription');
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardsList = [];
+
+// инициализируем массив начальным набором карточек
+cardsList.push(...initialCards);
+
+function renderElements() {
+  const elementTemplate = document.querySelector('#elementTemplate').content;
+  const elementsContainer = document.querySelector('.elements__container');
+
+  removeAllChildNodes(elementsContainer);
+
+    for(const card of cardsList) {
+    // клонируем содержимое тега template
+    const elementItem = elementTemplate.querySelector('.elements__item').cloneNode(true);
+
+    // наполняем содержимым
+    elementItem.querySelector('.elements__item-image').src = card.link;
+    elementItem.querySelector('.elements__title').textContent = card.name;
+
+    // отображаем на странице
+    elementsContainer.append(elementItem);
+  }
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
 function openEditProfilePopup() {
   editProfilePopup.classList.add('popup_opened');
 
@@ -35,3 +92,5 @@ function formSubmitHandler(evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 profileEditButton.addEventListener('click', openEditProfilePopup);
 editProfilePopupCloseButton.addEventListener('click', closeEditProfilePopup);
+
+renderElements();
